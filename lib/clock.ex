085f1f1c7b092d3b19f089e79d95512c3878170d
Naoki Nomoto <happygrind@gmail.com>
2015-11-14 23:28:58 +0900

    modify clock

--- lib/clock.ex
+++ lib/clock.ex
@@ -1,29 +1,44 @@
 defmodule Clock do
   use GenServer
 
-  def start(pid, func) do
-    GenServer.cast(pid, {:start_timer, pid, func})
+  def start(pid) do
+    GenServer.cast(pid, {:start_timer})
   end
 
   def stop(pid) do
     GenServer.cast(pid, {:stop_timer})
   end
 
-  def _timer_interval(func) do
-    func.()
+  def add_tick_handler(pid, listener) do
+    GenServer.cast(pid, {:add_tick_handler, listener})
+  end
+
+  def _timer_interval(event) do
+    GenEvent.notify(event, {:tick})
   end
 
   def start_link(ms \\ 1000) do
-    GenServer.start_link(__MODULE__, ms)
+    {:ok, event} = GenEvent.start_link
+    GenServer.start_link(__MODULE__, [ms, event])
   end
 
-  def init(ms) do
-    {:ok, {ms}}
+  def init([ms, event]) do
+    {:ok, {ms, event}}
   end
 
-  def handle_cast({:start_timer, pid, func}, {ms}) do
-    :timer.apply_interval(ms, __MODULE__, :_timer_interval, [func])
-    {:noreply, {ms}}
+  def handle_cast({:add_tick_handler, listener}, {ms, event}) do
+    GenEvent.add_handler(event, listener, [])
+    {:noreply, {ms, event}}
+  end
+
+  def handle_cast({:start_timer}, {ms, event}) do
+    {:ok, timer} = :timer.apply_interval(ms, __MODULE__, :_timer_interval, [event])
+    {:noreply, {ms, event, timer}}
+  end
+
+  def handle_cast({:stop_timer}, {ms, event, timer}) do
+    {:ok, :cancel} = :timer.cancel(timer)
+    {:noreply, {ms, event}}
   end
 end
 
