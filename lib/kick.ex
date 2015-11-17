41fdfbf6fd2d75d118fb60ae9bf4376b1dde20e2
Naoki Nomoto <happygrind@gmail.com>
2015-11-18 00:01:09 +0900

    add trigger message to instruments

--- lib/kick.ex
+++ lib/kick.ex
@@ -1,5 +1,29 @@
 defmodule Kick do
-  def play do
-    SC3.Server.send_msg("s_new", ["kick01", 1001])
+  use GenServer
+
+  def start_link do
+    GenServer.start_link(__MODULE__, [])
+  end
+
+  def init(_) do
+    {:ok, {}}
+  end
+
+  def play(pid) do
+    GenServer.cast(pid, {:play})
+  end
+
+  def handle_cast({:play}, _) do
+    SC3.Server.send_msg("s_new", ["kick01", SC3.Server.get_node_id])
+    {:noreply, {}}
+  end
+
+  def handle_cast({:trigger, 1}, _) do
+    SC3.Server.send_msg("s_new", ["kick01", SC3.Server.get_node_id])
+    {:noreply, {}}
+  end
+
+  def handle_cast({:trigger, _}, _) do
+    {:noreply, {}}
   end
 end
