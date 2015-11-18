484278d896360fb5d34836e2c8caa398f7ad7d94
Naoki Nomoto <happygrind@gmail.com>
2015-11-19 00:10:55 +0900

    add synth to p1

--- lib/p1.ex
+++ lib/p1.ex
@@ -5,17 +5,21 @@ defmodule P1 do
     {:ok, s1} = StepSequencer.start_link([1,0,0,1,0,0,0,1])
     {:ok, s2} = StepSequencer.start_link([0,0,0,0,0,0,1,0])
     {:ok, s3} = StepSequencer.start_link([1,1,1,1,1,1,1,1])
+    {:ok, s4} = StepSequencer.start_link([60,65,67,60,65,67,65,69])
     Clock.add_tick_handler(clock, s1)
     Clock.add_tick_handler(clock, s2)
     Clock.add_tick_handler(clock, s3)
+    Clock.add_tick_handler(clock, s4)
 
     {:ok, kick} = Kick.start_link
     {:ok, snare} = Snare.start_link
     {:ok, hat} = HiHat.start_link
+    {:ok, fm} = FmSynth.start_link
 
     StepSequencer.add_step_handler(s1, kick, :trigger)
     StepSequencer.add_step_handler(s2, snare, :trigger)
     StepSequencer.add_step_handler(s3, hat, :trigger)
+    StepSequencer.add_step_handler(s4, fm, :trigger)
 
     Clock.start(clock)
   end
