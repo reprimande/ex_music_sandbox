5e3d1b697678bbb1016f9605f1bbb386028afb83
Naoki Nomoto <happygrind@gmail.com>
2015-12-01 10:10:17 +0900

    add devide param to stream seq

--- lib/stream_sequencer.ex
+++ lib/stream_sequencer.ex
@@ -1,13 +1,13 @@
 defmodule StreamSequencer do
   use GenServer
 
-  def start_link(stream) do
-    GenServer.start_link(__MODULE__, [stream])
+  def start_link(stream, div \\ 1) do
+    GenServer.start_link(__MODULE__, [stream, div])
   end
 
-  def init([stream]) do
+  def init([stream, div]) do
     {:ok, event} = GenEvent.start_link
-    {:ok, %{ step: 0, stream: stream, event: event }}
+    {:ok, %{ step: 0, stream: stream, event: event, div: div }}
   end
 
   def add_step_handler(pid, listener, event_name) do
@@ -24,7 +24,13 @@ defmodule StreamSequencer do
   end
 
   def handle_cast({:tick}, state) do
-    GenEvent.notify(state.event, state.stream |> Enum.at(state.step))
+    if state.step == 0 || rem(state.step, state.div) == 0 do
+      GenEvent.notify(state.event, state.stream |> Enum.at(trunc(state.step / state.div)))
+    end
+    {:noreply, %{ step: state.step + 1, stream: state.stream, event: state.event, div: state.div }}
+  end
+
+  def handle_cast({:tick}, state) do
     {:noreply, %{ step: state.step + 1, stream: state.stream, event: state.event }}
   end
 end
