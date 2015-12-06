d3258fe2e7578bb1e3750d85911b1a1dd3ba8154
Naoki Nomoto <happygrind@gmail.com>
2015-12-07 02:03:13 +0900

    modify

--- mix.exs
+++ mix.exs
@@ -17,16 +17,7 @@ defmodule ExMusicSandbox.Mixfile do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    [{:exsc3, path: "../exsc3"}]
+    [{:exsc3, git: "https://github.com/naokinomoto/exsc3.git"}]
   end
 end
