5f2988a0d6bc496afd8c54c8dd671a8fe8115118
Naoki Nomoto <happygrind@gmail.com>
2015-12-09 23:52:51 +0900

    add rhythm track to generative piece.(but it's dirty code...)

--- lib/piece/generative_sup.ex
+++ lib/piece/generative_sup.ex
@@ -18,7 +18,22 @@ defmodule GenerativeTestSup do
     scale = [0,2,4,5,7,9,11,12,14,16,17,19,21,23,24]
 
     {:ok, sup} = Supervisor.start_link(__MODULE__, [])
-    {:ok, c} = Supervisor.start_child(sup, worker(Clock, [Clock.bpm2ms(80, 6)]))
+
+    {:ok, fm} = Supervisor.start_child(sup, worker(FmSynth, []))
+    {:ok, kick} = Supervisor.start_child(sup, worker(Kick, []))
+    {:ok, snare} = Supervisor.start_child(sup, worker(Snare, []))
+    {:ok, ch} = Supervisor.start_child(sup, worker(HiHat, [0.2], id: :ch1))
+    {:ok, oh} = Supervisor.start_child(sup, worker(HiHat, [0.8], id: :oh1))
+
+
+    perc_probs = [
+      { "k", kick,  [10, 0, 2, 0, 6, 0, 2, 0, 3, 0, 6, 0, 1, 0, 2, 4] },
+      { "s", snare, [0,  0, 1, 0, 1, 0, 6, 0, 6, 0, 1, 0, 1, 0, 6, 0] },
+      { "ch", ch,   [10, 2, 8, 2, 1, 2] },
+      { "oh", oh,   [0,  0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 0] }
+    ]
+
+    {:ok, c} = Supervisor.start_child(sup, worker(Clock, [Clock.bpm2ms(80, 12)]))
     {:ok, m} = Supervisor.start_child(sup, worker(Markov, [chord_chain, :I]))
     {:ok, l} = Supervisor.start_child(sup, worker(LogisticMap, [3.89, 0.1]))
 
@@ -27,6 +42,7 @@ defmodule GenerativeTestSup do
             fn (n) ->
               Markov.next_val(m)
               |> MidiUtil.atom2chord
+              |> MidiUtil.add_7th
               |> Enum.map(fn n ->
                 case n do
                   n when n > 7 -> n - 12
@@ -34,7 +50,7 @@ defmodule GenerativeTestSup do
                 end
               end)
               |> Enum.map(&(&1 + 60))
-          end, 6],
+          end, 12],
           id: :markov_seq))
 
     {:ok, s2} = Supervisor.start_child(sup, worker(
@@ -44,15 +60,31 @@ defmodule GenerativeTestSup do
                 1 -> 0
                 _ -> Enum.at(scale, trunc(LogisticMap.next_val(l) * length(scale))) + 60
               end
-            end],
+            end, 2],
           id: :logistic_seq))
 
-    {:ok, piano} = Supervisor.start_child(sup, worker(Piano, []))
+    perc_probs |> Enum.each(fn {n, i, p} ->
+      {:ok, s} = Supervisor.start_child(sup, worker(
+            StepSequencer, [
+            fn (n) ->
+              len = length(p)
+              index = rem(n, len)
+              val = p |> Enum.at(index)
+              case :rand.uniform(9) do
+                n when n < val -> 1
+                _ -> 0
+              end
+            end, 1],
+      id: n <> "_seq"))
+      Clock.add_tick_handler(c, s)
+      StepSequencer.add_step_handler(s, i, :trigger)
+    end)
 
     Clock.add_tick_handler(c, s1)
     Clock.add_tick_handler(c, s2)
-    StepSequencer.add_step_handler(s1, piano, :trigger)
-    StepSequencer.add_step_handler(s2, piano, :trigger)
+
+    StepSequencer.add_step_handler(s1, fm, :trigger)
+    StepSequencer.add_step_handler(s2, fm, :trigger)
     Clock.start(c)
 
     {:ok, sup}
--- lib/util/midi_util.ex
+++ lib/util/midi_util.ex
@@ -5,6 +5,10 @@ defmodule MidiUtil do
 
   def minor do [0, 3, 7] end
 
+  def add_7th([root|rest]) do
+    [root, rest, root + 10] |> List.flatten
+  end
+
   def atom2chord(atom) do
     case atom do
       :I   -> major
@@ -13,14 +17,14 @@ defmodule MidiUtil do
       :IV  -> major |> Enum.map(&(&1 + 6))
       :V   -> major |> Enum.map(&(&1 + 7))
       :VI  -> major |> Enum.map(&(&1 + 9))
-      :VII -> major |> Enum.map(&(&1 + 11))
+      :VII -> [11, 14, 17]
       :i   -> minor
       :ii  -> minor |> Enum.map(&(&1 + 2))
       :iii -> minor |> Enum.map(&(&1 + 4))
       :iv  -> minor |> Enum.map(&(&1 + 6))
       :v   -> minor |> Enum.map(&(&1 + 7))
       :vi  -> minor |> Enum.map(&(&1 + 9))
-      :vii -> minor |> Enum.map(&(&1 + 11))
+      :vii -> [11, 13, 17]
       _ -> []
     end
   end
